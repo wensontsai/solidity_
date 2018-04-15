4aef541f133d4e2117c5352e70026d5cd65668e4
Wenson Tsai <wensontsai@gmail.com>
2018-04-15 16:46:34 -0400

    adding web3, gitignore

--- inbox/compile.js
+++ inbox/compile.js
@@ -8,6 +8,6 @@ const source = fs.readFileSync(inboxPath, 'utf8');
 // Source and number of compile files.
 // Can compile multiple files in one go.
 // Exports object with compiled projects.
-// On each contract, `bytecode` and `interface`
+// On each contract, `bytecode` and `interface` (ABI)
 // Now only returning `:Inbox`
 module.exports = solc.compile(source, 1).contracts[':Inbox'];
---
+++ inbox/test/Inbox.test.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3'); // constructor function here.
+
+
+
+// (1) Deploy to local test network. 
+// Generate local network for testing only.
+// Ganache / Test RPC
+
+// (2) Web3 to interact with deployed contract on network.
+
