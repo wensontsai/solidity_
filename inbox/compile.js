d97cd5959924c3b6c8bd975afa34d1f55e39c9d3
Wenson Tsai <wensontsai@gmail.com>
2018-04-15 16:39:04 -0400

    adding compile.js

---
+++ inbox/compile.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+const source = fs.readFileSync(inboxPath, 'utf8');
+
+// Source and number of compile files.
+// Can compile multiple files in one go.
+// Exports object with compiled projects.
+// On each contract, `bytecode` and `interface`
+// Now only returning `:Inbox`
+module.exports = solc.compile(source, 1).contracts[':Inbox'];
