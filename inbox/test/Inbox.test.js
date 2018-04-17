11a2ba39ff1a1378a623db8738f75152c9b5e76e
Wenson Tsai <wensontsai@gmail.com>
2018-04-16 21:04:51 -0400

    first Ganache test

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -1,13 +1,32 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3'); // constructor function here.
+const web3 = new Web3(ganache.provider()); // instance
 
-// (1) Deploy to local test network. 
+// (1) Deploy contract to local test network. 
 // Generate local network for testing only.
 // Ganache / Test RPC
+// Accounts are unlocked for convenience.
+// Grab one for use in network.
 
 // (2) Web3 to interact with deployed contract on network.
 // Each instance can interact with another network.
 // Pass Provider, the communication layer to talk to network.
-const web3 = new Web3(ganache.provider());
+beforeEach(() => {
+    // Get a list of all 10 accounts for dev purposes.
+    web3.eth.getAccounts()
+        .then(fetchedAccounts => {
+            console.log(fetchedAccounts);
+        });
 
+    // Use one of accounts to deploy contract.
+    
+});
+
+// (3) Manipulate contract
+// (4) Assert contract change persisted and is correct.
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+
+    }); 
+});
